dad45306e7f85763b23fa6743106124260cda99e
shashidharpalli <shashidhar.g.palli@gmail.com>
2016-08-12 18:44:53 +0530

    view changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+
+//= require jquery-1.11.1.min
+//= require jquery.dataTables.min
 //= require jquery
 //= require jquery_ujs
+//= require local_time
 //= require_tree .
